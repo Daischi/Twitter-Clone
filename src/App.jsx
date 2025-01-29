609109ce0fc69d4dcbcd9087838d4db9a97796c6
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-01-28 22:53:55 -0300

    Primeiro commit, salvando projeto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import {Sidebar} from './components/Sidebar/index'
+function App() {
+  
+
+  return (
+    
+      <div className='flex mx-auto max-w-7xl'>
+        
+        
+        <Sidebar/>
+
+
+
+      </div>
+      
+   
+  )
+}
+
+export default App
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,22 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+export function Sidebar () {
+
+    return(
+
+        <div className="w-20 xl:w-64 sticky top-0 px-2 h-screen">
+
+
+        <FontAwesomeIcon icon={faTwitter} className='text-blue-400 text-3xl m-4'/>
+        
+
+        
+
+        </div>
+
+
+    )
+
+
+}
+
