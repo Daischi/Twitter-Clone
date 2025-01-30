0da7fdf020d240940fe953841e040b0e5469d424
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-01-30 16:35:29 -0300

    adicionando usuarios

--- src/components/Tweet/index.jsx
+++ src/components/Tweet/index.jsx
@@ -1,18 +1,50 @@
-export function Tweet ({tweet}){
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-    return(
+export function Tweet({ tweet }) {
+
+    return (
 
         <div className="border-b border-gray-800 p-4 hover:bg-gray-800 transition-all duration-300">
 
             <div className="flex space-x-3">
 
-                <img src={tweet.avatar} alt="user avatar"  className="rounded-full w-12 h-12"/>
+                <img src={tweet.avatar} alt="user avatar" className="rounded-full w-12 h-12" />
+                <div className="flex-1">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <span className="font-bold">
+
+                                {tweet.name}
+
+                            </span>
+                            <span className="text-gray-500 ml-2">
+
+                                @{tweet.username}
+
+                            </span>
+                            <span className="text-gray-500 ml-2">
+
+                            · {tweet.time}
+
+                            </span>
+
+                        </div>
+                        <FontAwesomeIcon icon={faEllipsisH} className="text-gray-500"/>
+                    </div>
+                    <p className="mt-2">
+                        {tweet.content}
+                    </p>
+                </div>
 
             </div>
 
 
+
+
+
         </div>
-    
+
 
 
     )
