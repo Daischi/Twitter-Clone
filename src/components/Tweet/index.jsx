097c755e27fa53cba1b9a097f8b01f0f467249f7
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-02-01 00:00:19 -0300

    adicinando useState

--- src/components/Tweet/index.jsx
+++ src/components/Tweet/index.jsx
@@ -1,11 +1,28 @@
 import { faChartBar, faComment, faEllipsisH, faHeader, faHeart, faLifeRing, faRetweet, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export function Tweet({ tweet }) {
+    const [comments, setComments] = useState(0)
+    const [like, setLikes] = useState(0)
+    const [retweet, setRetweet] = useState(0)
 
-    function handleAction(action){
+    function handleAction(action) {
+
+        switch (action) {
+            case 'like':
+                setLikes((like) => like + 1)
+                break;
 
 
+            case 'retweet':
+                setRetweet((retweet) => retweet + 1)
+                break;
+
+            default:
+                setComments((comments) => comments + 1)
+                break;
+        }
 
 
     }
@@ -32,48 +49,48 @@ export function Tweet({ tweet }) {
                             </span>
                             <span className="text-gray-500 ml-2">
 
-                            · {tweet.time}
+                                · {tweet.time}
 
                             </span>
 
                         </div>
-                        <FontAwesomeIcon icon={faEllipsisH} className="text-gray-500"/>
+                        <FontAwesomeIcon icon={faEllipsisH} className="text-gray-500" />
                     </div>
                     <p className="" >{tweet.content} </p>
-                        {tweet.Image && <img src={tweet.Image} className="mt-3 rounded-2xl max-w-full h-auto" alt="Imagem de conteudo do Usuario"/>}
+                    {tweet.Image && <img src={tweet.Image} className="mt-3 rounded-2xl max-w-full h-auto" alt="Imagem de conteudo do Usuario" />}
 
-                        <div className="flex justify-between mt-4 text-gray-500">
+                    <div className="flex justify-between mt-4 text-gray-500">
 
-                            <div className="flex items-center cursor-pointer hover:text-blue-400">
-                                <FontAwesomeIcon icon={faComment} onClick={() => handleAction('comment')}/>
-                                <span className="ml-2">0</span>
-
-                            </div>
-                            <div className="flex items-center cursor-pointer hover:text-green-300">
-                                <FontAwesomeIcon icon={faRetweet} onClick={() => handleAction('retweet')}/>
-                                <span className="ml-2">0</span>
-
-                            </div>
-
-                            <div className="flex items-center cursor-pointer hover:text-pink-300">
-                                <FontAwesomeIcon icon={faHeart} onClick={() => handleAction('like')}/>
-                                <span className="ml-2">0</span>
-
-                            </div>
-                            <div className="flex items-center cursor-pointer hover:text-pink-300">
-                                <FontAwesomeIcon icon={faChartBar} onClick={() => handleAction('like')}/>
-                                <span className="ml-2">0</span>
-
-                            </div>
-                            <div className="flex items-center cursor-pointer ">
-                                <FontAwesomeIcon icon={faUpload} />
-                                
-
-
-                            </div>
+                        <div className="flex items-center cursor-pointer hover:text-blue-400 transition-all duration-300">
+                            <FontAwesomeIcon icon={faComment} onClick={() => handleAction('comments')} />
+                            <span className="ml-2">{comments}</span>
 
                         </div>
-                    
+                        <div className="flex items-center cursor-pointer hover:text-green-300 transition-all duration-300">
+                            <FontAwesomeIcon icon={faRetweet} onClick={() => handleAction('retweet')} />
+                            <span className="ml-2">{retweet}</span>
+
+                        </div>
+
+                        <div className="flex items-center cursor-pointer hover:text-red-400 transition-all duration-300">
+                            <FontAwesomeIcon icon={faHeart} onClick={() => handleAction('like')} />
+                            <span className="ml-2">{like}</span>
+
+                        </div>
+                        <div className="flex items-center cursor-pointer hover:text-pink-300 transition-all duration-300">
+                            <FontAwesomeIcon icon={faChartBar} onClick={() => handleAction('')} />
+                            <span className="ml-2">0</span>
+
+                        </div>
+                        <div className="flex items-center cursor-pointer ">
+                            <FontAwesomeIcon icon={faUpload} />
+
+
+
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
